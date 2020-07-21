7dcb49873b1471a48b661ea6dfcda193c7f9a0f8
Vladislav Khmelevsky <och95@yandex.ru>
2020-07-21 21:27:51 +0300

    help.mk: Get makefiles list with dry run
    To prevent errors during makefile expansion get list mk with dry run

--- build_system/help.mk
+++ build_system/help.mk
@@ -8,15 +8,22 @@ RESET  := $(shell tput -Txterm sgr0)
 
 TARGET_MAX_CHAR_NUM := 20
 
+.PHONY: help_list
+# This target is used to get makefile list with makefile dry run
+help_list:
+	@$(MAKEFILE_LIST)
+
+.PHONY: help
 ## Show help
 help: $$ = $
 help:
+	@$(eval MK_LIST := $(shell $(MAKE) -n help_list))
 	@echo ''
 	@echo 'Usage:'
 	@echo '  ${YELLOW}make${RESET} ${GREEN}<target>${RESET}'
 	@echo ''
 	@echo 'Targets:'
-	@awk '/^[a-zA-Z0-9()_$$\/%@_-]+:/ { \
+	@awk '/^[a-zA-Z0-9()$$\/%@_-]+:/ { \
 		helpMessage = match(lastLine, /^## (.*)/); \
 		if (helpMessage) { \
 			helpCommand = substr($$1, 0, index($$1, ":")); \
@@ -24,4 +31,4 @@ help:
 			printf "  ${YELLOW}%-$(TARGET_MAX_CHAR_NUM)s${RESET} ${GREEN}%s${RESET}\n", helpCommand, helpMessage; \
 		} \
 	} \
-	{ lastLine = $$0 }' $(MAKEFILE_LIST)
+	{ lastLine = $$0 }' $(MK_LIST)
