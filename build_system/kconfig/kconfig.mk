60ba90e470198d801125d7fa5abdbd830f3c9abc
Vladislav Khmelevsky <och95@yandex.ru>
2024-12-22 17:51:42 +0400

    Add nconfig

--- build_system/kconfig/kconfig.mk
+++ build_system/kconfig/kconfig.mk
@@ -6,24 +6,33 @@ LXDIALOG_SRC := \
   lxdialog/util.c \
   lxdialog/yesno.c
 
-KCONFIG_SRC := \
+KCONFIG_COMMON := \
   confdata.c \
   expr.c \
   lexer.lex.c \
-  mconf.c \
   menu.c \
-  mnconf-common.c \
   parser.tab.c \
   preprocess.c \
   symbol.c \
   util.c
+
+MENUCONFIG_SRC := \
+  $(KCONFIG_COMMON) \
+  mconf.c \
+  mnconf-common.c
+
+NCONFIG_SRC := \
+  $(KCONFIG_COMMON) \
+  nconf.c \
+  nconf.gui.c \
+  mnconf-common.c
 
 $(eval $(call BUILD_LIBRARY, \
        $(CC_HOST), \
        HOST, \
        $(LXDIALOG_SRC), \
        $(KCONFIGDIR), \
-       lxdialog, \
+       lxdialoghost, \
        -fPIC -I$(KCONFIGDIR) -Wno-extra -Wno-all, \
 ))
 
@@ -31,16 +40,29 @@ ifneq ($(filter menuconfig, $(MAKECMDGOALS)),)
 $(eval $(call BUILD_EXECUTABLE, \
        $(CC_HOST), \
        HOST, \
-       $(KCONFIG_SRC), \
+       $(MENUCONFIG_SRC), \
        $(KCONFIGDIR), \
        menuconfig, \
        -fPIC -I$(KCONFIGDIR) -Wno-extra -Wno-all, \
 	   -pie,\
+	   -lncurses,\
 ))
 
 $(eval $(call NEEDED_LIBRARIES, \
        $(build_system/kconfig/menuconfig), \
-       $(build_system/kconfig/liblxdialog.a) \
-       -lncurses \
+       $(build_system/kconfig/liblxdialoghost.a) \
 ))
 endif # menuconfig
+
+ifneq ($(filter nconfig, $(MAKECMDGOALS)),)
+$(eval $(call BUILD_EXECUTABLE, \
+       $(CC_HOST), \
+       HOST, \
+       $(NCONFIG_SRC), \
+       $(KCONFIGDIR), \
+       nconfig, \
+       -fPIC -I$(KCONFIGDIR) -Wno-extra -Wno-all, \
+	   -pie,\
+	   -lmenu -lncurses -lpanel,\
+))
+endif # nconfig
